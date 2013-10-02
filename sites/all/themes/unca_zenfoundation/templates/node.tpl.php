40db2445078fd40f4b7634b50c436cc118ce4898
Lisa Jansen <lisajansen@serverlogic.com>
2013-10-02 14:57:47 -0700

    Adapted and renamed node template files (node, cluster, course, event, map item ) for d7 and resolved connflicts

--- sites/all/themes/unca_zenfoundation/templates/node.tpl.php
+++ sites/all/themes/unca_zenfoundation/templates/node.tpl.php
@@ -13,7 +13,7 @@
       <?php print render($title_prefix); ?>
       <?php if (!$page && $title): ?>
         <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-      <?php endif; ?>
+        <?php endif; ?>
       <?php print render($title_suffix); ?>
 
       <?php if ($display_submitted): ?>
@@ -37,7 +37,6 @@
   ?>
 
   <?php print render($content['links']); ?>
-
   <?php print render($content['comments']); ?>
 
 </article><!-- /node -->
