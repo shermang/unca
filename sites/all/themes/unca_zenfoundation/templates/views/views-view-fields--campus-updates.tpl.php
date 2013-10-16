fb8d3b4796355304eb76c5f0f1110334273385b6
Lisa Jansen <lisajansen@serverlogic.com>
2013-10-16 14:14:40 -0700

    #70: D7 Prototype: Copy the views template files (TPL files) from the D6 theme (campus updates)

---
+++ sites/all/themes/unca_zenfoundation/templates/views/views-view-fields--campus-updates.tpl.php
@@ -0,0 +1,4 @@
+<h3><?php print $fields['title']->content; ?></h3>
+	<?php print ($fields['field_campus_update_image_fid']->content)?> 
+	<p><?php print $fields['body']->content; ?></p>
+	<?php print $fields['view_node']->content; ?>
