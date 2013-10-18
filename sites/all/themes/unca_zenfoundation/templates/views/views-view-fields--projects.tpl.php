cb9044910a399ba5383ab8aa048caa6de82bb997
Lisa Jansen <lisajansen@serverlogic.com>
2013-10-17 22:11:49 -0700

    #70: D7 Prototype: Copy the views template files (TPL files): Project CT and Views updates

---
+++ sites/all/themes/unca_zenfoundation/templates/views/views-view-fields--projects.tpl.php
@@ -0,0 +1,6 @@
+<?php $base_path = base_path();?>
+<div class="profile">
+	<div class="profile-image"><?php print $fields['field_image']->content; ?></div>	
+	 <h2><?php print $fields['title']->content; ?></h2>
+	<?php print $fields['body']->content; ?></p>
+</div>
