2b115bd3ebe726f7ab71c61d9e1f26c0450ad3a0
Sherman Glass <shermanglass@serverlogic.com>
2013-10-24 11:49:48 -0700

    #14: Ph 2 - Content Migration to Subdomains added missing files from upgrade to migrate

---
+++ buildscripts/cleanm.sh
@@ -0,0 +1,19 @@
+drush dis --yes \
+migrate
+
+drush pm-uninstall --yes \
+migrate_d2d_unca
+
+
+drush pm-uninstall --yes \
+migrate_d2d
+
+
+drush pm-uninstall --yes \
+migrate_ui
+
+drush pm-uninstall --yes \
+migrate
+
+drush en --yes \
+migrate_d2d_unca
