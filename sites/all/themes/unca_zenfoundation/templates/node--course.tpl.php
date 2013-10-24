0b94b52cafc1102004d122fafd75fe66317b75bf
Marlene Williams <marlene@designbymarlene.com>
2013-10-24 12:04:53 -0700

    Theming changes to the Course content type.

--- sites/all/themes/unca_zenfoundation/templates/node--course.tpl.php
+++ sites/all/themes/unca_zenfoundation/templates/node--course.tpl.php
@@ -1,23 +1,26 @@
 <?php $base_path = base_path();?>
-<?php print render($content['field_image']); ?>
 
-<?php if ($page == 0): ?>
-<h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-<?php endif; ?>
 
- <div class="crn"><?php print render($content['field_course_number']); ?></div>
- <div class="course_dates"><?php print render($content['field_course_dates']); ?></div>
- <div class="times"><?php print render($content['field_times']); ?></div>
- <div class="course_instructor"><?php print render($content['field_instructor']) ?></div>
- <div class="catalog_term"><?php print render($content['field_course_catalog_term']) ?></div>
- <div class="days_term"><?php print render($content['field_course_days_term']) ?></div>
- <div class="times_term"><?php print render($content['field_course_times_term']) ?></div>
- <div class="topics_term"><?php print render($content['field_course_topics_term']) ?></div>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?><?php if (!$status) { print ' node-unpublished'; } ?>">
 
-<?php
-  $body = field_get_items('node',$node, 'body');
-  print $body[0]['value'];
-?>
+  <?php if ($page == 0): ?>
+  <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+  <?php endif; ?>
 
- <p><hr/><a href="http://olliasheville.com/courses">View our current courses and workshops.</a></p>
+  <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['comments']);
+    hide($content['links']);
+    print render($content);
+  ?>
+
+  <p>
+    <hr/>
+    <a href="http://olliasheville.com/courses">View our current courses and workshops.</a>
+  </p>
+
+  <?php print render($content['links']); ?>
+
+
+</div><!-- /.node -->
 
