73263f48ae9adae9b2e7773b06ad3e2508d048a8
Dan Muzyka <danmuzyka@serverlogic.com>
2013-12-05 13:35:49 -0800

    Adding the Flickr widget caption to the template file for Gateway pages Row 1. Ticket #126: Make the Gateway page layout match the layout of the existing gateway pages on Drupal 6

---
+++ sites/all/themes/unca_zenfoundation/templates/region--gateway-row-1.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Returns HTML for a region.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728112
+ */
+?>
+<?php if ($content): ?>
+  <div class="<?php print $classes; ?>">
+    <?php print $content; ?>
+    <div id="caption">Add your own images by joining the UNC Asheville <a href="http://www.flickr.com/groups/uncasheville/">Flickr group.</a></div>
+  </div>
+<?php endif; ?>
