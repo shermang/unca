4c1d3d80143ae0e3e74208d9c65678580fb2f6a7
Luke A. Withrow <lwithrow@cms7.unca.edu>
2014-02-07 10:19:50 -0500

    Added if (isset()) to a number of lines--this theme file appears to have not been updated with changes to the content type.

--- sites/all/themes/unca_zenfoundation/templates/views/views-view-fields--Project-SPIFF.tpl.php
+++ sites/all/themes/unca_zenfoundation/templates/views/views-view-fields--Project-SPIFF.tpl.php
@@ -1,8 +1,8 @@
 <div id="footer-profile">
-<img src="<?php print $fields['field_image']->content; ?>" id="profile-headshot"/>
-<p id="profile-name"><?php print $fields['title']->content; ?></p>
-<!-- <p id="profile-major"><?php print $fields['field_subhead']->content; ?></p> -->
-<p id="profile-abstract"><?php print $fields['field_teaser_text_value']->content; ?></p>
+<img src="<?php if (isset($fields['field_image'])) print $fields['field_image']->content; ?>" id="profile-headshot"/>
+<p id="profile-name"><?php if (isset($fields['title']))  print $fields['title']->content; ?></p>
+<!-- <p id="profile-major"><?php if (isset($fields['field_subhead'])) print $fields['field_subhead']->content; ?></p> -->
+<p id="profile-abstract"><?php if (isset($fields['field_teaser_text_value'])) print $fields['field_teaser_text_value']->content; ?></p>
 <a class="button-blue-rounded" href="/faces/<?php print strtolower($fields['name']->content); ?>"><span>Meet more of our <?php print strtolower($fields['name']->content); ?></span></a>
 <div class="clear">&nbsp;</div>
 </div>
