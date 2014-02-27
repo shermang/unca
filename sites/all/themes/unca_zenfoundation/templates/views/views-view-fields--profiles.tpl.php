49bc276495ffa8459a510468923807140759ff10
Sherman Glass <shermanglass@serverlogic.com>
2014-02-27 08:33:46 -0800

    #141: Profile CT display needs logic added

--- sites/all/themes/unca_zenfoundation/templates/views/views-view-fields--profiles.tpl.php
+++ sites/all/themes/unca_zenfoundation/templates/views/views-view-fields--profiles.tpl.php
@@ -7,5 +7,17 @@
  <?php } ?>
  <h2><?php print $fields['title']->content; ?></h2>
 <p><?php print $fields['field_subhead_value']->content; ?><br />
-<?php print $fields['field_teaser_text_value']->content; ?></p>
+<?php
+switch (TRUE){
+  case !empty($fields['field_profile_contact_info']->content):
+    print $fields['field_profile_contact_info']->content;
+    break;
+  case !empty($fields['field_profile_personal_statement']->content):
+    print $fields['field_profile_personal_statement']->content;
+    break;
+  default:
+    print $fields['field_teaser_text_value']->content;
+    break;
+}
+?></p>
 </div>
