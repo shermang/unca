f80b213261254ba2613a0ea7d6313bf07aaaf3b4
lwithrow <lwithrow@unca.edu>
2014-03-20 15:51:39 -0400

    Working on slideshow_item template to show image

--- sites/all/themes/unca_zenfoundation/templates/node--slideshow_item.tpl.php
+++ sites/all/themes/unca_zenfoundation/templates/node--slideshow_item.tpl.php
@@ -9,6 +9,8 @@
     // We hide the comments and links now so that we can render them later.
     hide($content['comments']);
     hide($content['links']);
+    print render($content['field_image']);
+    ?><hr/><?php
     print render($content);
   ?>
 
